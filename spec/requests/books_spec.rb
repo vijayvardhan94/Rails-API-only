bb1336ca712301cc789cab51307562f0311f78ff
vijayvardhan94 <tadimetv@oregonstate.edu>
2023-02-25 13:28:11 -0800

    test data with factory bot

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -3,7 +3,12 @@ require 'rails_helper'
 #create the high leveldesribe block, with the type of test -> in this case its a request spec
 describe 'Books API', type: :request do 
     it 'returns all books' do
+
+        FactoryBot.create(:book, author:'Phil Knight', title:'Shoe Dog')
+        FactoryBot.create(:book, author:'Zero to One', title:'Peter Thiel')
+
         get '/api/v1/books'
         expect(response).to have_http_status(:success)
+        expect(JSON.parse(response.body).size).to eq(2)
     end
 end
