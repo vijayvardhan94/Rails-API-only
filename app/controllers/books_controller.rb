db0442305606f5ccf80efd04c21e51430956091c
vijayvardhan94 <tadimetv@oregonstate.edu>
2023-02-24 20:13:57 -0800

    create method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,5 +2,23 @@ class BooksController < ApplicationController
   def index
     render json: Book.all
   end
+
+  def create
+    #Book.create
+    #title: params[:title], author: params[:author]
+    book = Book.new(book_params)
+
+    if book.save
+      render json: book, status: :created
+    else
+      render json: book.errors, status: :unprocessable_entity
+    end
+  end
+
+    private
+
+    def book_params
+      params.require(:book).permit(:title, :author)
+    end
 end
    
