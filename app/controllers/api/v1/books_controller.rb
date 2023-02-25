58a6d5631832b9f153dbff2e1c15f56ea35cdd78
vijayvardhan94 <tadimetv@oregonstate.edu>
2023-02-25 10:21:52 -0800

    namespacing and versioning

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,43 @@
+#rescue_From ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+module Api
+  module V1
+    class BooksController < ApplicationController
+  
+      def index
+        render json: Book.all, status: :ok
+      end
+
+      def create
+        #Book.create
+        #title: params[:title], author: params[:author]
+        book = Book.new(book_params)
+
+        if book.save
+          render json: book, status: :created
+        else
+          render json: book.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+
+        Book.find(params[:id]).destroy!
+        #to deal with exception use the destroy with a bang.
+        head :no_content
+
+      end
+
+        private
+
+        def book_params
+          params.require(:book).permit(:title, :author)
+        end
+
+        # def not_destroyed
+        #   render json: {}, status: :unprocessable_entity
+        # end
+    end
+  end
+end
+
+    
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  #get 'books/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :books, only: [:index, :create, :destroy]
+  namespace :api do
+    namespace :v1 do
+      resources :books, only: [:index, :create, :destroy]
+    end
+  end
   
 end
